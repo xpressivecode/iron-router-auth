be903b5cf20db291eaa5efd313850a59d13b067f
fuatsengul <fuat@fuatsengul.com.tr>
2014-09-02 18:09:39 +0300

    Update package.js

--- package.js
+++ package.js
@@ -1,5 +1,8 @@
 Package.describe({
     summary: "An iron-router module that gives you the ability to configure your routes so that only allow authenticated users may access them"
+    version: "1.0.0",
+    git: "https://github.com/fuatsengul/iron-router-auth"
+    
 });
 
 Package.on_use(function(api){
